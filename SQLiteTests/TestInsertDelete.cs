35574526f56f7f97821b785383a8a5f8ca1312a8
kootsoop <peter@kootsoop.com>
2020-04-14 07:37:54 -0400

    Fixed test.

--- SQLiteTests/TestInsertDelete.cs
+++ SQLiteTests/TestInsertDelete.cs
@@ -16,7 +16,7 @@ namespace SQLiteTests
         }
 
         [Test()]
-        public void Test1Insert()
+        public void TestInsert()
         {
             var item = new Item { Text = "Some Text", Description = "Some description" };
             var number_inserted = dataStore.AddItemAsync(item).Result;
@@ -25,13 +25,14 @@ namespace SQLiteTests
             Assert.AreEqual(item.Id, all_items[all_items.Count-1].Id);
         }
         [Test()]
-        public void Test2Delete()
+        public void TestDelete()
         {
             var all_items_before = dataStore.GetItemsAsync().Result.ToList<Item>();
 
             var item = new Item { Text = "Some Text", Description = "Some description" };
             var number_inserted = dataStore.AddItemAsync(item).Result;
             var deleted = dataStore.DeleteItemAsync(item.Id);
+            Assert.AreEqual(true, deleted);
 
             var all_items_after = dataStore.GetItemsAsync().Result.ToList<Item>();
             Assert.AreEqual(all_items_before.Count, all_items_after.Count);
